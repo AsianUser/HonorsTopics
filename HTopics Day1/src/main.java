fc4b9f844ae093fa801c330ca7c98379faaca2da
BenR <90284651+AsianUser@users.noreply.github.com>
2023-08-24 14:02:42 -0700

    Created Day1 Project
    Initialize the repository for a Java project with 2 functions:
    One that writes a String to a file
    One that reads from a file to a String

---
+++ HTopics Day1/src/main.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class main {
+
+    public void stringToFile(String fileName, String input) {
+
+        try {
+            PrintWriter pWrite = new PrintWriter(fileName);
+            // prepare
+            pWrite.write(input);
+
+            pWrite.close();
+        } catch (Exception e) {
+            System.out.println("bruh" + e);
+        }
+
+    }
+
+    public String fileToString(String fileName) {
+        String str = "";
+
+        try {
+            BufferedReader buffReader = new BufferedReader(new FileReader(fileName));
+
+            while (buffReader.ready()) {
+                int chara = buffReader.read();
+
+                // add chara to end of str
+                str += (char) chara;
+            }
+
+            buffReader.close();
+        } catch (Exception e) {
+            System.out.println("bruh" + e);
+        }
+
+        return str;
+    }
+
+}
